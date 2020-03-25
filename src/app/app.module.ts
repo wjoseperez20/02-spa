3c72c8612d27743b7dd6a723c3f4aa5ae3cf5759
Wilmer <wjoseperez20@gmail.com>
2020-03-24 22:02:34 -0400

    Work: add routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
+// Service
+
+// Routes
+import {APP_ROUTING} from './app.routes';
+
+// Components
 import {AppComponent} from './app.component';
 import {NavbarComponent} from './components/shared/navbar/navbar.component';
 import {HomeComponent} from './components/home/home.component';
-import { AboutComponent } from './components/about/about.component';
-import { HeroesComponent } from './components/heroes/heroes.component';
+import {AboutComponent} from './components/about/about.component';
+import {HeroesComponent} from './components/heroes/heroes.component';
+import {FooterComponent} from './components/shared/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -13,10 +20,12 @@ import { HeroesComponent } from './components/heroes/heroes.component';
     NavbarComponent,
     HomeComponent,
     AboutComponent,
-    HeroesComponent
+    HeroesComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
